5be557c7074c87678c2fb63e25c937c914c40c74
RY_2718 <ry.3607x5531@gmail.com>
2019-08-14 00:24:55 +0900

    Add `/login` page

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col xs="12">
+        <v-img
+          :src="require('@/assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col md="4" offset-md="4">
+        <v-form>
+          <v-text-field
+            label="メールアドレス"
+            placeholder="メールアドレス"
+            outlined=""
+          ></v-text-field>
+          <v-text-field
+            label="パスワード"
+            outlined=""
+            type="password"
+            aria-autocomplete="current-password"
+          ></v-text-field>
+          <v-btn block large>ログイン</v-btn>
+          <v-checkbox label="ログインしたままにする"></v-checkbox>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/pages/HelloWorld.vue';
+import Login from '@/components/pages/Login.vue';
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'hello-world',
       component: HelloWorld,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
   ],
 });
